30070b8c84d6b2bedd3f5ef828437ede178a6a11
emil <emilbabazade29@gmail.com>
2020-04-03 06:06:33 +0300

    ex 3.8 done

--- phonebook/index.js
+++ phonebook/index.js
@@ -3,9 +3,13 @@ const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
 
 //TODO: implement morgan middleware for logging
+
+morgan.token('body', (req, res) => JSON.stringify(req.body) )
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 
 //TODO set list of numbers
 let persons = [
