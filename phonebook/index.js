1533143fa67ef595b272827854583bbc95e46c8e
emil <emilbabazade29@gmail.com>
2020-04-03 03:30:16 +0300

    exercises 3.1, 3.2, 3.3

---
+++ phonebook/index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+
+const app = express()
+
+//TODO set list of numbers
+const persons = [
+    {
+      "name": "kamil",
+      "number": "98",
+      "id": 3
+    },
+    {
+      "name": "enef",
+      "number": "12",
+      "id": 4
+    },
+    {
+      "name": "emil",
+      "number": "123",
+      "id": 5
+    },
+    {
+      "name": "gud",
+      "number": "1234",
+      "id": 6
+    },
+    {
+      "name": "fefe",
+      "number": "01231",
+      "id": 7
+    }
+]
+
+//TODO implement get all
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+//TODO implement info page
+app.get('/info', (req, res) => {
+    const numOfPersons = persons.length
+    const now = new Date()
+    res.send(`
+        <p>Phonebook has info for ${numOfPersons} </p>
+        <p>${now}</p>
+    `)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+//TODO find person with id
+    const id = Number(req.params.id)
+    const person = persons.find(n => n.id === id)
+    if(person) {
+        res.json(person)
+    } else {
+//TODO if nothing send back 404 with error message
+        res.status(404).json({
+            error: `person with id ${id} does not exist`
+        })
+    }
+})
+
+const PORT = 3000
+app.listen(PORT)
+console.log(`server running on port ${PORT}`)
